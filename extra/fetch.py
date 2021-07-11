4bf0ffd177a423d76798be6e513202c6b215d8a7
0x1CA3 <86132648+0x1CA3@users.noreply.github.com>
2021-07-11 19:31:49 -0400

    Update fetch.py

--- extra/fetch.py
+++ extra/fetch.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-shodankey = "iiBjHE3TYIgavIGTrl6YIxJ0c6fiRQKz" # add your key here
+shodankey = "" # add your key here
 
 ip_req = requests.get(f"https://api.shodan.io/shodan/host/search?key={shodankey}&query=android+debug+bridge").text
 print(ip_req)
